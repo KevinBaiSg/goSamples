bd2f59eddbae66bcbbebd8c6b780e0a93377d075
Kevin Bai <kevin.bai.sin@gmail.com>
2019-05-24 14:33:08 +0800

    rename variable

--- string/str2bytes/str2bytes.go
+++ string/str2bytes/str2bytes.go
@@ -6,8 +6,8 @@ import (
 
 func str2bytes(s string) []byte {
 	x := (*[2]uintptr)(unsafe.Pointer(&s))
-	h := [3]uintptr{x[0], x[1], x[1]}
-	return *(*[]byte)(unsafe.Pointer(&h))
+	b := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&b))
 }
 
 func bytes2str(b []byte) string {
